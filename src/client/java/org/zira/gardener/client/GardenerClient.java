f3937bee355a223f932dc649dd9999892d526071
Karen <timoshchavlada@gmail.com>
2025-03-11 23:46:34 +0200

    Add initial project structure with Gradle setup, .gitignore, and license file Co-authored-by: Dmytro Vovk <VoDACode@users.noreply.github.com>

---
+++ src/client/java/org/zira/gardener/client/GardenerClient.java
@@ -0,0 +1,10 @@
+package org.zira.gardener.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GardenerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/zira/gardener/Gardener.java
@@ -0,0 +1,10 @@
+package org.zira.gardener;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gardener implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
